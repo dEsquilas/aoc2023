3900e854574fe0f06276c5843c8d13625a1bc1c9
Dani Esquilas <desquilas@gmail.com>
2023-12-03 12:49:35 +0100

    Refactor day03 extend

--- day03/main.py
+++ day03/main.py
@@ -1,27 +1,18 @@
 import pytest
 
 def day_3(filename):
-    matrix = [l.strip() for l in open(filename)]
 
-    square = [
-        (-1, -1),
-        (0, -1),
-        (1, -1),
-        (-1, 0),
-        (1, 0),
-        (-1, 1),
-        (0, 1),
-        (1, 1)
-    ]
+    matrix = [l.strip() for l in open(filename)]
+    square = [(-1, -1),(0, -1),(1, -1),(-1, 0),(1, 0),(-1, 1),(0, 1),(1, 1)]
 
     total_p1 = 0
     total_p2 = 0
-    asterix_values = {}
+    gear_values = {}
 
     for y in range(len(matrix)):
         current_number = ""
         current_number_is_valid = False
-        current_asterix = []
+        current_gear = []
         for x in range(len(matrix[y])):
             if matrix[y][x].isdigit():
                 current_number += matrix[y][x]
@@ -30,33 +21,33 @@ def day_3(filename):
                         if not matrix[y + dy][x + dx].isdigit() and matrix[y + dy][x + dx] != ".":
                             current_number_is_valid = True
                     if 0 <= x + dx < len(matrix[y]) and 0 <= y + dy < len(matrix) and  matrix[y + dy][x + dx] == "*":
-                        if not (y + dy, x + dx) in current_asterix:
-                            current_asterix.append((y + dy, x + dx))
+                        if not (y + dy, x + dx) in current_gear:
+                            current_gear.append((y + dy, x + dx))
             else:
                 if current_number_is_valid and len(current_number) > 0:
                     total_p1 += int(current_number)
                 if current_number != "":
-                    for asterix in current_asterix:
-                        if not asterix in asterix_values:
-                            asterix_values[asterix] = []
-                        asterix_values[asterix].append(int(current_number))
+                    for gear in current_gear:
+                        if not gear in gear_values:
+                            gear_values[gear] = []
+                        gear_values[gear].append(int(current_number))
 
-                current_asterix = []
+                current_gear = []
                 current_number = ""
                 current_number_is_valid = False
 
         if current_number_is_valid and len(current_number) > 0:
             total_p1 += int(current_number)
         if current_number != "":
-            for asterix in current_asterix:
-                if not asterix in asterix_values:
-                    asterix_values[asterix] = []
-                asterix_values[asterix].append(int(current_number))
-        current_asterix = []
+            for gear in current_gear:
+                if not gear in gear_values:
+                    gear_values[gear] = []
+                gear_values[gear].append(int(current_number))
+        current_gear = []
         current_number = ""
         current_number_is_valid = False
 
-    for asterix, values in asterix_values.items():
+    for gear, values in gear_values.items():
         if len(values) == 2:
             total_p2 += values[0] * values[1]
 
